988cb96e03e2da47da92629618cafcb304ce1bd2
Ondicho <albert.ondicho@moringaschool.com>
2022-06-06 13:09:36 +0300

    commit tree 14f926cf3de4a2d52778716d274e0b596865be90

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+function findMatching(array, variable) {
+  let newArray = [];
+  for (let item of array) {
+    if (item === variable) {
+      newArray.push(item);
+      console.log(newArray)
+    } else if (item.toLowerCase() === variable) {
+      newArray.push(item);
+      console.log(newArray)
+    }
+  }
+  return newArray;
+}
+
+function fuzzyMatch(array,variable){
+    let newArray=[];
+    for (let item of array){
+        
+        return newArray
+    }
+}
+
+function matchName(array,variable){
+    filter(array,function(item){
+        
+        return item.name===variable;
+    })
+}
